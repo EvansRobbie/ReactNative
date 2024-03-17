dd78fd343fd08d4017ce7fae8d620514d34b0921
EvansRobbie <evansrobbie5311@gmail.com>
2024-03-17 03:32:19 +0300

    Text Component

--- CoreConcepts/App.js
+++ CoreConcepts/App.js
@@ -1,12 +1,18 @@
-import {View} from "react-native"
-const App = () =>{
-  return(
-    <View style={{flex:1, backgroundColor:"red"}}>
+import { View, Text } from "react-native";
+const App = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "red", padding: 60 }}>
       {/* View is similar to div but no scrollable */}
-      <View style={{width:200,height:200, backgroundColor:"blue"}}/>
-      <View style={{width:200,height:200, backgroundColor:"lightgreen"}}/>
+      {/* <View style={{width:200,height:200, backgroundColor:"blue"}}/>
+      <View style={{width:200,height:200, backgroundColor:"lightgreen"}}/> */}
+      {/* Text is similar to p */}
+      {/* all texts must be inside a Text else you will run into an error */}
+      <Text>
+        <Text style={{ color: "white" }}>Hello</Text>
+        World
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
